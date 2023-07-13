28ef1e665f6ae2c94186780d61c8cfc194a4a00e
minyogog <139486592+minyogog@users.noreply.github.com>
2023-07-13 18:25:17 +0100

    Update streamlit_app.py

--- cameroonian/keyboard/streamlit_app.py
+++ cameroonian/keyboard/streamlit_app.py
@@ -1,11 +1,11 @@
 # african keyboards 
 button_labels = [
    “0”, “1”, “2”, “3”, “4”, “5”, “6”, “7”, “8”, “9”
-  “a”, “z”, “e”, “r”, “t”, “y”, “u”, “I”, “o”, “p“
+  “a”, “z”, “e”, “r”, “t”, “y”, “u”, “I”, “o”, "p"
   “s”, “d”, “f”, “g”, “h”, “j”, “k”, “l”, “m”
     w”, “c”, “v”, “b”, “ɓ”,  “ɔ” 
     “capitalize(upload image from path "C:\Users\bideg\Pictures\fleche_jb.jpg")”, “m”, “n”, “ny”, “ŋ”, “backspace(upload image from path "C:\Users\bideg\Pictures\backspace_jb.jpg")”] with each button background color yellow, text font color black"
-“?”, “ǃ”, “space”, “,” , “;”, “.”, “Enter (upload image from path "C:\Users\bideg\Pictures\enter_jb.jpg")”] with each button background color yellow, text font color black
+“?”, “ǃ”, “space”, “,” , “;”, "."  "Enter (upload image from path "C:\Users\bideg\Pictures\enter_jb.jpg")”] with each button background color yellow, text font color black
 # Create a placeholder to store the selected buttons
 selected_buttons = []
 
